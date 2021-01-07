e22ee2ee9d7b841c7d34264838ec5d39796f3edd
pkro <pkrombach@web.de>
2021-01-07 06:45:51 +0100

    fixed board width for  uneven length

--- exercises/01/chess.js
+++ exercises/01/chess.js
@@ -1,6 +1,8 @@
 'use strict';
 
-const size = 10;
+const size = 4;
 for (let i = 1; i <= size; i++) {
-  console.log((i % 2 == 0 ? '# ' : ' #').repeat(Math.round(size / 2)));
+  console.log(
+    (i % 2 == 0 ? '# ' : ' #').repeat(Math.round(size / 2)).substr(0, size)
+  );
 }
